e9d215047ec0a84a3b8ce12d3d7d6dcb25247f18
Dario Ramos <dario.fenix@gmail.com>
2017-08-01 20:09:14 -0300

    DEV-8: Uses spaces, not tabs

--- build.py
+++ build.py
@@ -6,12 +6,12 @@ if __name__ == "__main__":
     builder.add_common_builds(shared_option_name="bitprim-database:shared")
     builder.password = os.getenv("CONAN_PASSWORD")
 
-	filtered_builds = []
-	for settings, options, env_vars, build_requires in builder.builds:
-		if settings["build_type"] == "Release" \
-				and settings["arch"] == "x86_64" \
-				and options["bitprim-consensus:shared"] == False:
-			filtered_builds.append([settings, options, env_vars, build_requires])
+    filtered_builds = []
+    for settings, options, env_vars, build_requires in builder.builds:
+        if settings["build_type"] == "Release" \
+                and settings["arch"] == "x86_64" \
+                and options["bitprim-consensus:shared"] == False:
+            filtered_builds.append([settings, options, env_vars, build_requires])
 
     builder.builds = filtered_builds
     builder.run()
