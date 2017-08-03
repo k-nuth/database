eac1938028fbbc93876a441482b3fff5589a5152
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-08-03 11:12:33 -0300

    conan try remotes in python code

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ class BitprimdatabaseConan(ConanFile):
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
-    exports_sources = "src/*", "CMakeLists.txt", "cmake/*", "bitprim-databaseConfig.cmake.in", "include/*"
+    exports_sources = "src/*", "CMakeLists.txt", "cmake/*", "bitprim-databaseConfig.cmake.in", "include/*", "test/*"
     package_files = "build/lbitprim-database.a"
     build_policy = "missing"
 
