513acf6e2bd7e11230421bd69f9c64fbfd528aa5
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-08-17 13:50:05 -0300

    conan improvements

--- conanfile.py
+++ conanfile.py
@@ -1,15 +1,55 @@
+#
+# Copyright (c) 2017 Bitprim developers (see AUTHORS)
+#
+# This file is part of Bitprim.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from conans import ConanFile, CMake
 
+def option_on_off(option):
+    return "ON" if option else "OFF"
 
-class BitprimdatabaseConan(ConanFile):
+class BitprimDatabaseConan(ConanFile):
     name = "bitprim-database"
     version = "0.1"
     license = "http://www.boost.org/users/license.html"
     url = "https://github.com/bitprim/bitprim-database/tree/conan-build/conanfile.py"
     description = "Bitcoin High Performance Blockchain Database"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
+
+    # options = {"shared": [True, False]}
+    # default_options = "shared=False"
+
+    options = {"shared": [True, False],
+               "fPIC": [True, False],
+               "with_tests": [True, False],
+               "with_tools": [True, False],
+    }
+
+    default_options = "shared=False", \
+        "fPIC=True", \
+        "with_tests=True", \
+        "with_tools=True"
+
+# option(USE_CONAN "Use Conan Build Tool." OFF)
+# option(ENABLE_SHARED "" OFF)
+# option(WITH_TESTS "Compile with unit tests." ON)
+# option(WITH_TOOLS "Compile with tools." OFF)
+
+
     generators = "cmake"
     exports_sources = "src/*", "CMakeLists.txt", "cmake/*", "bitprim-databaseConfig.cmake.in", "include/*", "test/*"
     package_files = "build/lbitprim-database.a"
@@ -20,7 +60,14 @@ class BitprimdatabaseConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
+        
+        cmake.definitions["USE_CONAN"] = "ON"
         cmake.definitions["CMAKE_VERBOSE_MAKEFILE"] = "ON"
+        cmake.definitions["ENABLE_SHARED"] = option_on_off(self.options.shared)
+        cmake.definitions["ENABLE_POSITION_INDEPENDENT_CODE"] = option_on_off(self.options.fPIC)
+        cmake.definitions["WITH_TESTS"] = option_on_off(self.options.with_tests)
+        cmake.definitions["WITH_TOOLS"] = option_on_off(self.options.with_tools)
+
         cmake.configure(source_dir=self.conanfile_directory)
         cmake.build()
 
