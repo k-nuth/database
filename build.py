da6852e677144427f968c77a9367eec4129464e3
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-08-18 15:36:58 -0300

    to stable channel

--- build.py
+++ build.py
@@ -1,9 +1,9 @@
 from conan.packager import ConanMultiPackager
-import os
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="bitprim", channel="testing",
-                                 remotes="https://api.bintray.com/conan/bitprim/bitprim")
+    builder = ConanMultiPackager(username="bitprim", channel="stable",
+                                 remotes="https://api.bintray.com/conan/bitprim/bitprim",
+                                 archs=["x86_64"])
 
     builder.add_common_builds(shared_option_name="bitprim-database:shared")
     # builder.password = os.getenv("CONAN_PASSWORD")
@@ -11,7 +11,6 @@ if __name__ == "__main__":
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
         if settings["build_type"] == "Release" \
-                and settings["arch"] == "x86_64" \
                 and options["bitprim-database:shared"] == False:
             filtered_builds.append([settings, options, env_vars, build_requires])
 
