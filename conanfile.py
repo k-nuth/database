651ec0874dacb97cebf0284dac0315a7fc6adfa6
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-09-15 10:55:52 -0300

    specific branches and version changes

--- conanfile.py
+++ conanfile.py
@@ -24,7 +24,7 @@ def option_on_off(option):
 
 class BitprimDatabaseConan(ConanFile):
     name = "bitprim-database"
-    version = "0.1"
+    version = "0.2"
     license = "http://www.boost.org/users/license.html"
     url = "https://github.com/bitprim/bitprim-database/tree/conan-build/conanfile.py"
     description = "Bitcoin High Performance Blockchain Database"
@@ -52,7 +52,7 @@ class BitprimDatabaseConan(ConanFile):
     build_policy = "missing"
 
     requires = (("bitprim-conan-boost/1.64.0@bitprim/stable"),
-                ("bitprim-core/0.1@bitprim/testing"))
+                ("bitprim-core/0.2@bitprim/testing"))
 
     def build(self):
         cmake = CMake(self)
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -8,7 +8,7 @@ username = os.getenv("CONAN_USERNAME", "bitprim")
 
 class BitprimdatabaseTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "bitprim-database/0.1@%s/%s" % (username, channel)
+    requires = "bitprim-database/0.2@%s/%s" % (username, channel)
     generators = "cmake"
 
     def build(self):
