50d05dbf24b5cecf9f6b7ede3e4db065d86393b3
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-07-04 19:38:39 -0300

    Feature ci adjust compilers (#15)
    CI changes

--- include/bitcoin/database/version.hpp
+++ include/bitcoin/database/version.hpp
@@ -4,34 +4,24 @@
 //        GENERATED SOURCE CODE, DO NOT EDIT EXCEPT EXPERIMENTALLY
 //
 ///////////////////////////////////////////////////////////////////////////////
-#ifndef LIBBITCOIN_DATABASE_VERSION_HPP
-#define LIBBITCOIN_DATABASE_VERSION_HPP
+#ifndef LIBBITCOIN_DATABASE_VERSION_HPP_
+#define LIBBITCOIN_DATABASE_VERSION_HPP_
 
 /**
  * The semantic version of this repository as: [major].[minor].[patch]
  * For interpretation of the versioning scheme see: http://semver.org
  */
 
-#define LIBBITCOIN_DATABASE_VERSION "0.11.0"
-#define LIBBITCOIN_DATABASE_MAJOR_VERSION 0
-#define LIBBITCOIN_DATABASE_MINOR_VERSION 11
-#define LIBBITCOIN_DATABASE_PATCH_VERSION 0
-
-// #define STR_HELPER(x) #x
-// #define STR(x) STR_HELPER(x)
-// #define LIBBITCOIN_DATABASE_VERSION STR(LIBBITCOIN_DATABASE_MAJOR_VERSION) "." STR(LIBBITCOIN_DATABASE_MINOR_VERSION) "." STR(LIBBITCOIN_DATABASE_PATCH_VERSION)
-// #undef STR
-// #undef STR_HELPER
-
-#ifdef BITPRIM_BUILD_NUMBER
-#define BITPRIM_DATABASE_VERSION BITPRIM_BUILD_NUMBER
+#ifdef BITPRIM_PROJECT_VERSION
+#define BITPRIM_DATABASE_VERSION BITPRIM_PROJECT_VERSION
 #else
-#define BITPRIM_DATABASE_VERSION "v0.0.0"
+#define BITPRIM_DATABASE_VERSION "0.0.0"
 #endif
+
 
 namespace libbitcoin { namespace database {
 char const* version();
 }} /*namespace libbitcoin::database*/
  
 
-#endif
+#endif // LIBBITCOIN_DATABASE_VERSION_HPP_
