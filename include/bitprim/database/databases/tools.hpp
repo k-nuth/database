6b6a1f7c1bc63509775d34a608ce986a85f5123d
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-10-08 11:02:24 -0300

    fix linking in blockchain

--- include/bitprim/database/databases/tools.hpp
+++ include/bitprim/database/databases/tools.hpp
@@ -29,25 +29,30 @@ namespace database {
 //Note: same logic as is_stale()
 
 template <typename Clock>
+inline
 std::chrono::time_point<Clock> to_time_point(std::chrono::seconds secs) {
     return std::chrono::time_point<Clock>(typename Clock::duration(secs));
 }
 
 template <typename Clock>
+inline
 bool is_old_block_(uint32_t header_ts, std::chrono::seconds limit) {
     return (Clock::now() - to_time_point<Clock>(std::chrono::seconds(header_ts))) >= limit;
 }
 
 template <typename Clock>
+inline
 bool is_old_block_(chain::block const& block, std::chrono::seconds limit) {
     return is_old_block_<Clock>(block.header().timestamp(), limit);
 }
 
-constexpr 
+constexpr
+inline
 std::chrono::seconds blocks_to_seconds(uint32_t blocks) {
     return std::chrono::seconds(blocks * target_spacing_seconds);   //10 * 60
 }
 
+inline
 data_chunk db_value_to_data_chunk(MDB_val const& value) {
     return data_chunk{static_cast<uint8_t*>(value.mv_data), 
                       static_cast<uint8_t*>(value.mv_data) + value.mv_size};
