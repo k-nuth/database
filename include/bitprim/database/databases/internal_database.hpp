5f50aaa2e03eac9f3cf704959d7f611398e3367e
Mario <the4power@yahoo.com.ar>
2018-10-24 15:57:10 -0300

    same environment

--- include/bitprim/database/databases/internal_database.hpp
+++ include/bitprim/database/databases/internal_database.hpp
@@ -62,7 +62,6 @@ public:
 
     internal_database_basis(path const& db_dir, uint32_t reorg_pool_limit, uint64_t db_max_size)
         : db_dir_(db_dir)
-        , db_dir_blocks_(db_dir / "_blocks")
         , reorg_pool_limit_(reorg_pool_limit)
         , limit_(blocks_to_seconds(reorg_pool_limit))
         , db_max_size_(db_max_size)
@@ -89,17 +88,6 @@ public:
             return false;
         }
 
-
-        if ( ! create_directories(db_dir_blocks_, ec)) {
-            if (ec.value() == directory_exists) {
-                LOG_ERROR(LOG_DATABASE) << "Failed because the directory " << db_dir_blocks_ << " already exists.";
-                return false;
-            }
-
-            LOG_ERROR(LOG_DATABASE) << "Failed to create directory " << db_dir_blocks_ << " with error, '" << ec.message() << "'.";
-            return false;
-        }
-
         return open();
     }    
 
@@ -108,11 +96,7 @@ public:
             return false;
         }
 
-        if ( ! create_and_open_environment_blocks_db()) {
-            return false;
-        }
-
-        return open_databases() && open_databases_blocks_db();
+        return open_databases();
     }
 
     bool close() {
@@ -124,7 +108,7 @@ public:
             mdb_dbi_close(env_, dbi_reorg_index_);
             mdb_dbi_close(env_, dbi_reorg_block_);
             
-            mdb_dbi_close(env_block_, dbi_block_db_);
+            mdb_dbi_close(env_, dbi_block_db_);
             
             db_opened_ = false;
         }
@@ -132,8 +116,6 @@ public:
         if (env_created_) {
             mdb_env_close(env_);
             
-            mdb_env_close(env_block_);    
-    
             env_created_ = false;
         }
 
@@ -200,7 +182,7 @@ public:
         MDB_val value {valuearr.size(), valuearr.data()};
 
         MDB_txn* db_txn;
-        auto res = mdb_txn_begin(env_block_, NULL, 0, &db_txn);
+        auto res = mdb_txn_begin(env_, NULL, 0, &db_txn);
         if (res != MDB_SUCCESS) {
             LOG_INFO(LOG_DATABASE) << "Error open transaction LMDB Block [push_block_db] " << res;
             return result_code::other;
@@ -460,42 +442,6 @@ private:
         return res == MDB_SUCCESS;
     }
 
-    bool create_and_open_environment_blocks_db() {
-
-        if (mdb_env_create(&env_block_) != MDB_SUCCESS) {
-            return false;
-        }
-        
-        // TODO(fernando): see what to do with mdb_env_set_maxreaders ----------------------------------------------
-        // int threads = tools::get_max_concurrency();
-        // if (threads > 110 &&	/* maxreaders default is 126, leave some slots for other read processes */
-        //     (result = mdb_env_set_maxreaders(m_env, threads+16)))
-        //     throw0(DB_ERROR(lmdb_error("Failed to set max number of readers: ", result).c_str()));
-        // ----------------------------------------------------------------------------------------------------------------
-
-        auto res = mdb_env_set_mapsize(env_block_, adjust_db_size(db_max_size_*2));
-        if (res != MDB_SUCCESS) {
-            return false;
-        }
-
-        res = mdb_env_set_maxdbs(env_block_, 1);
-        if (res != MDB_SUCCESS) {
-            return false;
-        }
-
-        //TODO(fernando): check if we can apply the following flags
-        // if (db_flags & DBF_FASTEST)
-        //     mdb_flags |= MDB_NOSYNC | MDB_WRITEMAP | MDB_MAPASYNC;
-
-        res = mdb_env_open(env_block_, db_dir_blocks_.string().c_str() , MDB_NORDAHEAD | MDB_NOSYNC | MDB_NOTLS, env_open_mode_);
-     
-        if (res != MDB_SUCCESS) {
-            return false;
-        }
-
-        return true;
-    }
-
     bool open_databases() {
         MDB_txn* db_txn;
         auto res = mdb_txn_begin(env_, NULL, 0, &db_txn);
@@ -532,30 +478,15 @@ private:
             return false;
         }
 
-        db_opened_ = mdb_txn_commit(db_txn) == MDB_SUCCESS;
-        return db_opened_;
-    }
 
-    bool open_databases_blocks_db() {
-        
-        MDB_txn* db_txn;
-        auto res = mdb_txn_begin(env_block_, NULL, 0, &db_txn);
-        if (res != MDB_SUCCESS) {
-            return false;
-        }
-        
         res = mdb_dbi_open(db_txn, block_db_name, MDB_CREATE | MDB_INTEGERKEY, &dbi_block_db_);
         if (res != MDB_SUCCESS) {
             return false;
         }
 
-        res = mdb_txn_commit(db_txn);
-        if (res != MDB_SUCCESS) {
-            return false;
-        }
 
-        return true;
-
+        db_opened_ = mdb_txn_commit(db_txn) == MDB_SUCCESS;
+        return db_opened_;
     }
 
     result_code insert_reorg_pool(uint32_t height, MDB_val& key, MDB_txn* db_txn) {
@@ -1114,7 +1045,6 @@ private:
 
     // Data members ----------------------------
     path const db_dir_;
-    path const db_dir_blocks_;
     uint32_t reorg_pool_limit_;                 //TODO(fernando): check if uint32_max is needed for NO-LIMIT???
     std::chrono::seconds const limit_;
     bool env_created_ = false;
@@ -1130,7 +1060,6 @@ private:
     MDB_dbi dbi_reorg_block_;
 
     //Blocks DB
-    MDB_env* env_block_;
     MDB_dbi dbi_block_db_;
 
 };
