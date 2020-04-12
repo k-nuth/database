82af0d9f8c54d9d9441f873c7c85492309e0e718
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-12 17:21:22 -0300

    uses fmt lib instead of boost format

--- tools/initchain/initchain.cpp
+++ tools/initchain/initchain.cpp
@@ -3,52 +3,53 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <iostream>
-#include <boost/format.hpp>
+
+// #include <boost/format.hpp>
 #include <boost/filesystem.hpp>
+
+#include <fmt/core.h>
+
 #include <kth/database.hpp>
 
-#define BS_INITCHAIN_DIR_NEW \
-    "Failed to create directory %1% with error, '%2%'.\n"
-#define BS_INITCHAIN_DIR_EXISTS \
-    "Failed because the directory %1% already exists.\n"
-#define BS_INITCHAIN_FAIL \
-    "Failed to initialize database files.\n"
+// #define BS_INITCHAIN_DIR_NEW "Failed to create directory %1% with error, '%2%'.\n"
+// #define BS_INITCHAIN_DIR_EXISTS "Failed because the directory %1% already exists.\n"
+#define BS_INITCHAIN_DIR_NEW "Failed to create directory {} with error, '{}'.\n"
+#define BS_INITCHAIN_DIR_EXISTS "Failed because the directory {} already exists.\n"
+#define BS_INITCHAIN_FAIL "Failed to initialize database files.\n"
 
 using namespace bc;
 using namespace bc::chain;
 using namespace bc::database;
 using namespace boost::filesystem;
 using namespace boost::system;
-using boost::format;
+// using boost::format;
 
 // Create a new mainnet database.
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     std::string prefix("mainnet");
 
-    if (argc > 1)
+    if (argc > 1) {
         prefix = argv[1];
+    }
 
-    if (argc > 2 && std::string("--clean") == argv[2])
+    if (argc > 2 && std::string("--clean") == argv[2]) {
         remove_all(prefix);
+    }
 
     error_code code;
-    if (!create_directories(prefix, code))
-    {
-        if (code.value() == 0)
-            std::cerr << format(BS_INITCHAIN_DIR_EXISTS) % prefix;
-        else
-            std::cerr << format(BS_INITCHAIN_DIR_NEW) % prefix %
-                code.message();
-
+    if ( ! create_directories(prefix, code)) {
+        if (code.value() == 0) {
+            std::cerr << fmt::format(BS_INITCHAIN_DIR_EXISTS, prefix);
+        } else {
+            std::cerr << fmt::format(BS_INITCHAIN_DIR_NEW, prefix, code.message());
+        }
         return -1;
     }
 
     // This creates default configuration database only!
     const settings configuration;
 
-    if (!data_base(configuration).create(block::genesis_mainnet()))
-    {
+    if ( ! data_base(configuration).create(block::genesis_mainnet())) {
         std::cerr << BS_INITCHAIN_FAIL;
         return -1;
     }
