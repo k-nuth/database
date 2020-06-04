2baa02f3323e7d89f57671fec531826d3ece0e90
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-04 20:25:00 -0300

    use Catch2 framework in memory_map

--- src/legacy/memory/memory_map.cpp
+++ src/legacy/memory/memory_map.cpp
@@ -218,7 +218,7 @@ bool memory_map::flush() const
     ///////////////////////////////////////////////////////////////////////////
 
     // Keep logging out of the critical section.
-    if (!error_name.empty())
+    if ( ! error_name.empty())
         return handle_error(error_name, filename_);
 
     ////log_flushed();
