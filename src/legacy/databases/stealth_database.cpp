37c051a44fd2a9c4c6620757918d6749d27a6f42
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-08 19:33:00 -0300

    use Catch2 framework in stealth_database

--- src/legacy/databases/stealth_database.cpp
+++ src/legacy/databases/stealth_database.cpp
@@ -11,10 +11,9 @@
 #include <kth/domain.hpp>
 #include <kth/database/legacy/memory/memory.hpp>
 
-namespace kth {
-namespace database {
+namespace kth::database {
 
-using namespace bc::chain;
+using namespace kth::domain::chain;
 
 static constexpr auto rows_header_size = 0u;
 
