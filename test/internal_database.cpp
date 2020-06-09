96265d2ad7f027a44c6a1b18822654810f80921d
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-08 23:42:00 -0300

    Never use Boost.Test again internal_database

--- test/internal_database.cpp
+++ test/internal_database.cpp
@@ -318,9 +318,9 @@ void check_blocks_db_doesnt_exists(KTH_DB_env* env_, KTH_DB_dbi& dbi_blocks_db_,
     auto key = kth_db_make_value(sizeof(height), &height);
     KTH_DB_val value;
 
-    BOOST_REQUIRE(kth_db_txn_begin(env_, NULL, KTH_DB_RDONLY, &db_txn) == KTH_DB_SUCCESS);
-    BOOST_REQUIRE(kth_db_get(db_txn, dbi_blocks_db_, &key, &value) == KTH_DB_NOTFOUND);
-    BOOST_REQUIRE(kth_db_txn_commit(db_txn) == KTH_DB_SUCCESS);
+    REQUIRE(kth_db_txn_begin(env_, NULL, KTH_DB_RDONLY, &db_txn) == KTH_DB_SUCCESS);
+    REQUIRE(kth_db_get(db_txn, dbi_blocks_db_, &key, &value) == KTH_DB_NOTFOUND);
+    REQUIRE(kth_db_txn_commit(db_txn) == KTH_DB_SUCCESS);
 }
 
 #endif
