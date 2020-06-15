ec546c8002e9ff3329671cd1c382f73725e3fcfd
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-15 20:51:00 -0300

    Never use Boost.Test again allocator

--- src/legacy/memory/allocator.cpp
+++ src/legacy/memory/allocator.cpp
@@ -32,7 +32,7 @@ uint8_t* allocator::buffer()
 // Add an unsafe offset to share-protected pointer (convenience method).
 void allocator::increment(size_t value)
 {
-    KTH_ASSERT((size_t)data_ <= bc::max_size_t - value);
+    KTH_ASSERT((size_t)data_ <= kth::max_size_t - value);
     data_ += value;
 }
 
