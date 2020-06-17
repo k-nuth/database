453df27a8f6e161253e1bfcbaca0f76c24fe3c50
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-16 23:49:00 -0300

    Never use Boost.Test again internal_database

--- test/internal_database.cpp
+++ test/internal_database.cpp
@@ -2588,13 +2588,13 @@ TEST_CASE("internal database  prune", "[None]") {
     #endif
     ) = open_dbs();
 
-    BOOST_REQUIRE(db_count_items(env_, dbi_reorg_pool_) == 2);
-    BOOST_REQUIRE(db_count_items(env_, dbi_reorg_index_) == 2);
-    BOOST_REQUIRE(db_count_items(env_, dbi_reorg_block_) == 2);
+    REQUIRE(db_count_items(env_, dbi_reorg_pool_) == 2);
+    REQUIRE(db_count_items(env_, dbi_reorg_index_) == 2);
+    REQUIRE(db_count_items(env_, dbi_reorg_block_) == 2);
 
-    BOOST_REQUIRE(db_count_items(env_, dbi_utxo_) == 8);
-    BOOST_REQUIRE(db_count_items(env_, dbi_block_header_) == 8);
-    BOOST_REQUIRE(db_count_items(env_, dbi_block_header_by_hash_) == 8);
+    REQUIRE(db_count_items(env_, dbi_utxo_) == 8);
+    REQUIRE(db_count_items(env_, dbi_block_header_) == 8);
+    REQUIRE(db_count_items(env_, dbi_block_header_by_hash_) == 8);
     
     
      #if defined(KTH_DB_NEW_BLOCKS) 
