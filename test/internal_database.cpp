6e040783ee6f5a4be6f1baa5efe3740123925825
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-16 23:18:00 -0300

    Remove Boost testing framework in internal_database

--- test/internal_database.cpp
+++ test/internal_database.cpp
@@ -2526,13 +2526,13 @@ TEST_CASE("internal database  prune", "[None]") {
     #endif
     ) = open_dbs();
 
-    BOOST_REQUIRE(db_count_items(env_, dbi_reorg_pool_) == 1);
-    BOOST_REQUIRE(db_count_items(env_, dbi_reorg_index_) == 1);
-    BOOST_REQUIRE(db_count_items(env_, dbi_reorg_block_) == 1);
+    REQUIRE(db_count_items(env_, dbi_reorg_pool_) == 1);
+    REQUIRE(db_count_items(env_, dbi_reorg_index_) == 1);
+    REQUIRE(db_count_items(env_, dbi_reorg_block_) == 1);
 
-    BOOST_REQUIRE(db_count_items(env_, dbi_utxo_) == 7);
-    BOOST_REQUIRE(db_count_items(env_, dbi_block_header_) == 7);
-    BOOST_REQUIRE(db_count_items(env_, dbi_block_header_by_hash_) == 7);
+    REQUIRE(db_count_items(env_, dbi_utxo_) == 7);
+    REQUIRE(db_count_items(env_, dbi_block_header_) == 7);
+    REQUIRE(db_count_items(env_, dbi_block_header_by_hash_) == 7);
 
      #if defined(KTH_DB_NEW_BLOCKS) 
     check_blocks_db(env_, dbi_block_db_,0);
