9b920be91be3afdc12ee4e0429f8336a956f8779
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-16 22:29:00 -0300

    Replace Boost.Test with Catch2 in internal_database

--- test/internal_database.cpp
+++ test/internal_database.cpp
@@ -2465,13 +2465,13 @@ TEST_CASE("internal database  prune", "[None]") {
     #endif
     ) = open_dbs();
 
-    BOOST_REQUIRE(db_count_items(env_, dbi_reorg_pool_) == 0);
-    BOOST_REQUIRE(db_count_items(env_, dbi_reorg_index_) == 0);
-    BOOST_REQUIRE(db_count_items(env_, dbi_reorg_block_) == 0);
+    REQUIRE(db_count_items(env_, dbi_reorg_pool_) == 0);
+    REQUIRE(db_count_items(env_, dbi_reorg_index_) == 0);
+    REQUIRE(db_count_items(env_, dbi_reorg_block_) == 0);
 
-    BOOST_REQUIRE(db_count_items(env_, dbi_utxo_) == 6);
-    BOOST_REQUIRE(db_count_items(env_, dbi_block_header_) == 6);
-    BOOST_REQUIRE(db_count_items(env_, dbi_block_header_by_hash_) == 6);
+    REQUIRE(db_count_items(env_, dbi_utxo_) == 6);
+    REQUIRE(db_count_items(env_, dbi_block_header_) == 6);
+    REQUIRE(db_count_items(env_, dbi_block_header_by_hash_) == 6);
 
 
     #if defined(KTH_DB_NEW_BLOCKS) 
