37214ade7e4b3957a48c1d716592bcbdc005602e
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-18 23:20:00 -0300

    use Catch2 in transaction_result

--- include/kth/database/legacy/result/transaction_result.hpp
+++ include/kth/database/legacy/result/transaction_result.hpp
@@ -51,10 +51,10 @@ public:
     bool is_spent(size_t fork_height) const;
 
     /// The output at the specified index within this transaction.
-    chain::output output(uint32_t index) const;
+    domain::chain::output output(uint32_t index) const;
 
     /// The transaction, optionally including witness.
-    chain::transaction transaction(bool witness = true) const;
+    domain::chain::transaction transaction(bool witness = true) const;
 
 private:
     memory_ptr slab_;
