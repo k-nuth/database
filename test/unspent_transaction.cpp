87eb8aa919ac410edb23cc9fa61e74a159c35a57
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-21 22:44:00 -0300

    Never use Boost.Test again unspent_transaction

--- test/unspent_transaction.cpp
+++ test/unspent_transaction.cpp
@@ -34,66 +34,56 @@ BOOST_AUTO_TEST_CASE(unspent_transaction__copy__coinbase_tx_hash_height__expecte
     BOOST_REQUIRE_EQUAL(copied.is_coinbase(), true);
 }
 
-BOOST_AUTO_TEST_CASE(unspent_transaction__construct1__null_hash__null_hash)
-{
+BOOST_AUTO_TEST_CASE(unspent_transaction__construct1__null_hash__null_hash) {
     BOOST_REQUIRE(unspent_transaction(null_hash).hash() == null_hash);
 }
 
-BOOST_AUTO_TEST_CASE(unspent_transaction__construct1__tx_hash__expected_hash)
-{
+BOOST_AUTO_TEST_CASE(unspent_transaction__construct1__tx_hash__expected_hash) {
     static const transaction tx;
     static auto const expected = tx.hash();
     BOOST_REQUIRE(unspent_transaction(expected).hash() == expected);
 }
 
-BOOST_AUTO_TEST_CASE(unspent_transaction__construct2__point_hash__expected_hash)
-{
+BOOST_AUTO_TEST_CASE(unspent_transaction__construct2__point_hash__expected_hash) {
     static const transaction tx;
     static auto const expected = tx.hash();
     static const output_point point{ expected, 42 };
     BOOST_REQUIRE(unspent_transaction(point).hash() == expected);
 }
 
-BOOST_AUTO_TEST_CASE(unspent_transaction__construct3__tx__expected_hash)
-{
+BOOST_AUTO_TEST_CASE(unspent_transaction__construct3__tx__expected_hash) {
     static const transaction tx;
     BOOST_REQUIRE(unspent_transaction(tx, 0, 0, false).hash() == tx.hash());
 }
 
-BOOST_AUTO_TEST_CASE(unspent_transaction__outputs__construct1__tx_hash__empty)
-{
+BOOST_AUTO_TEST_CASE(unspent_transaction__outputs__construct1__tx_hash__empty) {
     static const transaction tx;
     auto const expected = tx.hash();
     BOOST_REQUIRE(unspent_transaction(expected).outputs()->empty());
 }
 
-BOOST_AUTO_TEST_CASE(unspent_transaction__outputs__construct2__empty)
-{
+BOOST_AUTO_TEST_CASE(unspent_transaction__outputs__construct2__empty) {
     static const transaction tx;
     const output_point point{ tx.hash(), 42 };
     BOOST_REQUIRE(unspent_transaction(point).outputs()->empty());
 }
 
-BOOST_AUTO_TEST_CASE(unspent_transaction__outputs__construct3_empty_tx__empty)
-{
+BOOST_AUTO_TEST_CASE(unspent_transaction__outputs__construct3_empty_tx__empty) {
     static const transaction tx;
     BOOST_REQUIRE(unspent_transaction(tx, 0, 0, false).outputs()->empty());
 }
 
-BOOST_AUTO_TEST_CASE(unspent_transaction__outputs__construct3_single_output_tx__one_output)
-{
+BOOST_AUTO_TEST_CASE(unspent_transaction__outputs__construct3_single_output_tx__one_output) {
     static const transaction tx{ 0, 0, {}, { {} } };
     BOOST_REQUIRE_EQUAL(unspent_transaction(tx, 0, 0, false).outputs()->size(), 1u);
 }
 
-BOOST_AUTO_TEST_CASE(unspent_transaction__equal__tx_hash_only__true)
-{
+BOOST_AUTO_TEST_CASE(unspent_transaction__equal__tx_hash_only__true) {
     static const transaction tx;
     BOOST_REQUIRE(unspent_transaction(tx, 0, 0, false) == unspent_transaction(tx.hash()));
 }
 
-BOOST_AUTO_TEST_CASE(unspent_transaction__move_assign__coinbase_tx_hash_height__expected)
-{
+BOOST_AUTO_TEST_CASE(unspent_transaction__move_assign__coinbase_tx_hash_height__expected) {
     static const transaction tx{ 0, 0, { { { null_hash, point::null_index }, {}, 0 } }, {} };
     static auto const expected_height = 42u;
     unspent_transaction instance(tx, expected_height, 0, false);
