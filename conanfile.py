a1a6b2e03799d0d6260c52216fd934ae5df8ffc1
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-10-13 12:00:47 -0300

    use spdlog by default

--- conanfile.py
+++ conanfile.py
@@ -60,7 +60,7 @@ class KnuthDatabaseConan(KnuthConanFile):
         "cflags": "_DUMMY_",
         "glibcxx_supports_cxx11_abi": "_DUMMY_",
         "cmake_export_compile_commands": False,
-        "log": "boost",
+        "log": "spdlog",
         "use_libmdbx": False,
     }
 
