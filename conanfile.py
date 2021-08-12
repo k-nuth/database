971e3e1a21fa10ebe6e405eb734e800a34ed6d3c
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-08-12 18:08:26 -0300

    upgrade dependencies

--- conanfile.py
+++ conanfile.py
@@ -86,7 +86,7 @@ class KnuthDatabaseConan(KnuthConanFile):
             self.output.info("Using legacy DB")
 
         if self.options.tests:
-            self.requires("catch2/2.13.1@")
+            self.requires("catch2/2.13.7")
 
         self.requires("domain/0.X@%s/%s" % (self.user, self.channel))
 
