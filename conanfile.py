f47cdc6b87fb4444def3ff69b0844d47cd1a917b
Fernando Pelliccioni <fpelliccioni@gmail.com>
2023-04-05 00:43:13 +0200

    test conan transitive deps

--- conanfile.py
+++ conanfile.py
@@ -64,12 +64,7 @@ class KnuthDatabaseConan(KnuthConanFileV2):
             self.test_requires("catch2/3.3.1")
 
     def requirements(self):
-        # self.requires("infrastructure/0.23.0")
         self.requires("domain/0.25.0", transitive_headers=True, transitive_libs=True)
-
-        # self.requires("boost/1.81.0")
-        # self.requires("fmt/9.1.0")
-        # self.requires("spdlog/1.11.0")
 
         if not self._is_legacy_db():
             if self.options.use_libmdbx:
