4e4859e5e60ea920566ba407b830266fbeb7a2d1
Fernando Pelliccioni <fpelliccioni@gmail.com>
2025-06-10 16:38:53 +0200

    release v0.51.0

--- conanfile.py
+++ conanfile.py
@@ -62,7 +62,7 @@ class KnuthDatabaseConan(KnuthConanFileV2):
             self.test_requires("catch2/3.7.1")
 
     def requirements(self):
-        self.requires("domain/0.44.0", transitive_headers=True, transitive_libs=True)
+        self.requires("domain/0.45.0", transitive_headers=True, transitive_libs=True)
 
         if self.options.use_libmdbx:
             self.requires("libmdbx/0.7.0@kth/stable", transitive_headers=True, transitive_libs=True)
